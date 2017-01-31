2c1dccd3a0b6b0a597b734a12f87d5238d75eb40
alex-blair <alex.n.blair@gmail.com>
2017-01-31 21:14:54 +1300

    test js

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ Translate a given word or phrase from English into Maori using the Google Transl
 - how to include a space in the url?
 
 1) Set up the elements
-  
+
+  $.ajaxSend(function(event, xhr, settings) {
+    /* stuff to do before an AJAX request is sent */;
+  });
+
+  $.$.ajaxSetup({
+    url: '/path/to/file',
+    type: 'GET',
+    dataType: 'json',
+    data: {param1: 'value1'},
+  })
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
