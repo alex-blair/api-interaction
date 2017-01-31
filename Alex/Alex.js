f26bae74b3d14b237b447b97642fb9abd714fe5f
alex-blair <alex.n.blair@gmail.com>
2017-02-01 11:00:24 +1300

    add basic css styling to different sections

--- Alex/Alex.js
+++ Alex/Alex.js
@@ -1,19 +1,19 @@
-// Include a document ready function to ensure the functions render after HTML has loaded
+// Include a document ready function to ensure the functions render after HTML has loaded:
 $(document).ready(function() {
 
-  // Attach a submit handler to the form
+  // Attach a submit handler to the form:
   $("#form").submit(function(e) {
+    // Stop form from submitting normally
     e.preventDefault()
     // Get some values from elements on the page:
-    // 1 Return the value attribute of #engligh-text
+    // 1 Return the value attribute of #engligh-text i.e. the text we want to translate:
     let textToTranslate = $("#form").find("#english-text").val()
-    // 2 Define "url" as the url we wish to request combined with the text we wish to translate
+    // 2 Add the textToTranslate to the end of the API url so that when we send off a request we will get a corresponding translation back:
     let url = "https://eda-te-reo.herokuapp.com/api/translate?word=" + textToTranslate
 
-    // Send the request to translate using $.get(url, callback), where "url" is the url we wish to request (defined above), and "callback" is the name of a function to be executed if the request succeeds
+    // Use $.get(url, callback) to translate the text, where "url" is the url we wish to request (defined above), and "callback" is the name of a function to be executed if the request succeeds:
     $.get(url, function(translation) {
-
-    //Remove all child nodes (including text nodes) from the result div before inserting the new translation into the result div
+    //Remove all child nodes (including text nodes) from the result div before inserting the new translation into the result div:
       $("#result").empty().append(translation);
     });
   })
