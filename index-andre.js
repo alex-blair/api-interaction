4c11010421645d318c21e3b373afc6a1728ef71e
Andre Charbonneau <andre@thematrix.nzgit config --global user.name Andre>
2017-02-01 11:51:10 +1300

    sets up ajax response assignment

--- index-andre.js
+++ index-andre.js
@@ -1,17 +1,22 @@
 var input = "Seafood is very very good"
 
+var response = [];
+
 $.ajax({
   url: 'https://eda-te-reo.herokuapp.com/api/translate',
   type: 'GET',
-  dataType: 'json',
+  dataType: 'text',
   data: {"word": input },
-  // success: function(message)
+  success: function(data) {
+    console.log('success', data)
+    response.push(data);
+  }
 })
-.done(function() {
-  console.log("success");
-})
+// .done(function() {
+//   console.log("success". res);
+// })
 .fail(function() {
-  console.log("error");
+  console.log("error", err);
 })
 .always(function() {
   console.log("complete");
