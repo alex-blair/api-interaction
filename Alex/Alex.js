3d33559294959fedf566f6c82aed9b58c53eb84a
alex-blair <alex.n.blair@gmail.com>
2017-02-02 09:33:33 +1300

    add further comments to Alex.js

--- Alex/Alex.js
+++ Alex/Alex.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+  // $("#form").click(function(event) {
   $("#form").submit(function(event) {
     event.preventDefault()
-    let textToTranslate = $("#form").find("#english-text").val()
+    let textToTranslate = $("#english-text").val()
     let url = "https://eda-te-reo.herokuapp.com/api/translate?word=" + textToTranslate
     $.ajax({
       url: url,
@@ -11,7 +12,7 @@ $(document).ready(function() {
         if(data.includes('Error')) {
           handleError(data)
         } else {
-          // document.getElementByID("result").innerHTML = data;
+          // document.getElementById("result").innerHTML = data;
           $("#result").html(data);
         }
       }
@@ -23,6 +24,45 @@ let handleError = function (err) {
   $("#result").html("Oops! " + err);
 }
 
+// //***********************************************************
+//
+// // Include a document ready function to ensure the functions render after HTML has loaded:
+// $(document).ready(function() {
+//   // Attach a submit handler to the form (this is a function that will execute each time a user attempts to submit the form. A user can submit a form by clicking <button type="submit">, or by pressing Enter, so in this case if the user presses enter instead of clicking the Translate Now button the function will still execute):
+//   $("#form").submit(function(event) {
+//     // Stop form from submitting normally so that the function will run when the user attempts to submit the form:
+//     event.preventDefault()
+//     // Get some values from elements on the page:
+//     // 1 Return the value the text we want to translate:
+//     let textToTranslate = $("#form").find("#english-text").val()
+//     // 2 Define new url variable so that the text we want to translate is added to the end of the API url when we make a request:
+//     let url = "https://eda-te-reo.herokuapp.com/api/translate?word=" + textToTranslate
+//     // 3 Use Ajax function
+//     $.ajax({
+//       url: url,
+//       type: 'GET',
+//       error: handleError,
+//       success: function(data) {
+//         if(data.includes('Error')) {
+//           handleError(data)
+//         } else {
+//           //Insert the new translation into a new div with the id "result" using .html(), which is the jQuery way of writing document.getElementById("result").innerHTML = data;
+//           $("#result").html(data);
+//         }
+//       }
+//     })
+//   })
+// })
+//
+// //Change the default message that the server sends when an error occurs
+// let handleError = function (err) {
+//   $("#result").html("Oops! " + err);
+// }
+
+// //***********************************************************
+//
+// // Another way of running the Ajax function using jQuery .get() - a shorthand Ajax function:
+//
 // $(document).ready(function() {
 //   $("#form").submit(function(event) {
 //     event.preventDefault()
@@ -33,6 +73,8 @@ let handleError = function (err) {
 //     });
 //   })
 // })
+//
+// //***********************************************************
 //
 // // Include a document ready function to ensure the functions render after HTML has loaded:
 // $(document).ready(function() {
@@ -49,7 +91,7 @@ let handleError = function (err) {
 //
 //     // Use $.get(url, callback) to translate the text, where "url" is the url we wish to request (defined above), and "callback" is the name of a function to be executed if the request succeeds:
 //     $.get(url, function(translation) {
-//     //Remove all child nodes (including text nodes) from the result div using .empty() before inserting the new translation into the result div using .append():
+//     //Insert the new translation into the result div using .html():
 //       $("#result").html(translation);
 //     });
 //   })
